ec2eb78693617aa67dca558e14e5d4f2b76e7eb6
RMSanchez <raulsg93@gmail.com>
2018-07-23 23:20:31 -0500

    cli table and inventoryadded

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,12 @@
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var table = new Table({
+    chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+           , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+           , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+           , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
+  });
 
 var mysql = require("mysql");  //download these using npm 
 
@@ -14,4 +22,29 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    connection.query("SELECT * FROM products", function(err, response){
+        if(err) throw err;
+        let titles = ["item_id","product_name","department_name","price","stock"];
+        table.push(titles);
+        console.log(response[1]);
+        
+        for(let i =0;i<response.length;i++){
+            var newRow = [];
+            newRow.push(response[i].item_id);
+            newRow.push(response[i].product_name);
+            newRow.push(response[i].department_name);
+            newRow.push(response[i].price);
+            newRow.push(response[i].stock_quantity);
+            table.push(newRow);           
+        }
+        console.log(table.toString());
+        //DISPLAY TABLE AND EVERYTHING BEFORE PROMPT
+        inquirer.prompt([
+            {
+                name: "action",
+                
+            }
+        ])
+    });
 });
+
